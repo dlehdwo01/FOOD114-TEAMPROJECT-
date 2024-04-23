9d164994c95d0622f309ef305f26b8f715892e77
dlehdwo01 <djl01@naver.com>
2024-04-23 16:09:43 +0900

    server 구축을 위한 setting

--- food114/src/main/java/teamProject/food114/Food114Application.java
+++ food114/src/main/java/teamProject/food114/Food114Application.java
@@ -2,9 +2,15 @@ package teamProject.food114;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Food114Application {
+public class Food114Application extends SpringBootServletInitializer{// 추가 코드
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Food114Application.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Food114Application.class, args);
