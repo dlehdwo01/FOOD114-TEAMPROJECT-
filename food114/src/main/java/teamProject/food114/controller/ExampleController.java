0f1fffe62de1b33af2e12082faea25458cd4a57b
이동재 <153072974+dlehdwo01@users.noreply.github.com>
2024-05-17 11:30:57 +0900

    Update ExampleController.java

--- food114/src/main/java/teamProject/food114/controller/ExampleController.java
+++ food114/src/main/java/teamProject/food114/controller/ExampleController.java
@@ -37,7 +37,7 @@ public class ExampleController {
 
     public ExampleController() {
         // 반드시 계정 내 등록된 유효한 API 키, API Secret Key를 입력해주셔야 합니다!
-        this.messageService = NurigoApp.INSTANCE.initialize("NCS9MUHWM1TTQBIM", "K4XULEFQS1MI5XI6JROFIBBEC2LC7ONT", "https://api.coolsms.co.kr");
+        this.messageService = NurigoApp.INSTANCE.initialize("", "", "https://api.coolsms.co.kr");
     }
     
     /**
